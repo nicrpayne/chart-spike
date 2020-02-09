696d32bdf6cb25dff97935d0aeb7378fa9adc48b
Nic Payne <nic.r.payne@gmail.com>
2020-02-09 12:34:51 -0600

    adding in chartjs data and testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from '../src/components/ChartComponent'
 
 class App extends Component {
   render() {
--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js';
-require(['moment'], function () {
-    require(['chartjs'], function (Chart) {
-        new Chart(ctx,);
-    });
+// require(['moment'], function () {
+//     require(['chartjs'], function (Chart) {
+//         new Chart(ctx,);
+//     });
+// });
+
+let myChart = document.getElementById('myChart').getContext('2d');
+let testChart = new Chart(myChart, {
+    type: 'bar', //bar, horizontalBar, pie, line, donut,  radar, polar area
+    data: {
+        labels: ['Minneapolis', 'St. Paul', 'Rochester', 'Duluth', ],
+        datasets: [{
+            label: 'population',
+            data: [
+                425403, 
+                307695, 
+                116961, 
+                85884
+            ]
+
+        }],
+
+    },
+    options: { }
 });
 
 class Chart extends Component {
+
+
+    
     render() {
+
+       
+
         return (
+            
             <>
+            
+            <body>
+                <div className="container">
+                    <canvas id="myChart"></canvas>
+                    
+                </div>
+
+            </body>
             </>
         );
     }

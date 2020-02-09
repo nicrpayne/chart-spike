d520d75399c26db2d4474cc61c6d41c3b517c749
Nic Payne <nic.r.payne@gmail.com>
2020-02-09 11:59:51 -0600

    initial commit

---
+++ src/components/ChartComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Chart from 'chart.js';
+require(['moment'], function () {
+    require(['chartjs'], function (Chart) {
+        new Chart(ctx,);
+    });
+});
+
+class Chart extends Component {
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default Chart;
